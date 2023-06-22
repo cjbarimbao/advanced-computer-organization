47fea81f17187dfba122066937201e8790da7636
CJ <christian.barimbao@eee.upd.edu.ph>
2023-06-23 00:27:26 +0800

    update jtest

--- MP 2/tests/jtest.asm
+++ MP 2/tests/jtest.asm
@@ -1,13 +1,34 @@
 ld x20,8(x0)
 add x10,x0,x0
-jal x0,28
+jal x0, jump1
+addi x0, x0, 0
+addi x0, x0, 0
 ld x10,0(x0)
+jump2:
 add x10,x11,x10
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
 add x10,x10,x10
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
 add x10,x10,x10
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
 add x1,x0,x20
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
 jalr x0,-8(x1)
+jump1:
 ld x11,0(x0)
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
 sd x11,24(x0)
-jal x1,-28
+jal x1,jump2
+addi x0, x0, 0
+addi x0, x0, 0
 sd x10,16(x0)
--- MP 2/tests/ldtest.asm
+++ MP 2/tests/ldtest.asm
@@ -1,36 +1,34 @@
-ld x6,0(x0)
-ld x21,8(x0)
-ld x22,8(x0)
-ld x23,16(x0)
+ld x20,8(x0)
+add x10,x0,x0
+jal x0, jump1
 addi x0, x0, 0
 addi x0, x0, 0
-beq x21,x22, branch1
-addi x0, x0, 0
-addi x0, x0, 0
-add x6,x6,x6
-branch1: 
-sd x6,24(x0)
-beq x21,x23, branch2
-addi x0, x0, 0
-addi x0, x0, 0
-add x6,x6,x6
-branch2: 
+ld x10,0(x0)
+jump2:
+add x10,x11,x10
 addi x0, x0, 0
 addi x0, x0, 0
 addi x0, x0, 0
-sd x6,32(x0)
-bne x22,x23, branch3
-addi x0, x0, 0
-addi x0, x0, 0
-add x6,x6,x6
-branch3:
-sd x6,40(x0)
-bne x22,x21, branch4
-addi x0, x0, 0
-addi x0, x0, 0
-add x6,x6,x6
-branch4:
+add x10,x10,x10
 addi x0, x0, 0
 addi x0, x0, 0
 addi x0, x0, 0
-sd x6,48(x0)
+add x10,x10,x10
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+add x1,x0,x20
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+jalr x0,-8(x1)
+jump1:
+ld x11,0(x0)
+addi x0, x0, 0
+addi x0, x0, 0
+addi x0, x0, 0
+sd x11,24(x0)
+jal x1,jump2
+addi x0, x0, 0
+addi x0, x0, 0
+sd x10,16(x0)
